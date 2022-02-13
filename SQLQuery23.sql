4da14a05e7f54f1ae6d4651f1973cdb52df4a6ab
freakyrishi <95204461+freakyrishi@users.noreply.github.com>
2022-02-13 21:35:46 +0530

    Add files via upload

---
+++ SQLQuery23.sql
@@ -0,0 +1,3 @@
+create table voot(rollno int,userid int, name varchar(20))
+insert into voot values(22,100,'karan')
+select*from voot
---
+++ SQLQuery24.sql
@@ -0,0 +1,6 @@
+create table no220000(id int,name varchar(20),city varchar(20))
+insert into no220000 values(222,'rishi','nagpur')
+insert into no220000 values(333,'rekha','delhi')
+insert into no220000 values(444,'ravindra','pune')
+insert into no220000 values(555,'rachit','nagpur')
+select distinct * from no220000
